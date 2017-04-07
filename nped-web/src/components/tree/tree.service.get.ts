03c1f0af28c05c51a607b5ca6338e1f60baa3fec
Rodolfo Pazos Rivas <rodopazos@gmail.com>
2017-04-07 09:44:48 -0400

    Add tree component folder and data

---
+++ nped-web/src/components/tree/tree.service.get.ts
@@ -0,0 +1,26 @@
+// Angular Imports
+import { Http, Response } from '@angular/http';
+import { Injectable, Inject } from '@angular/core';
+
+// OPT Imports
+import { ComponentServiceGet } from '../opt/classes/class.service.get';
+import { IComponentServiceConfig } from '../opt/interfaces/interface.service.config';
+
+// Others Imports
+import { Observable } from 'rxjs/Rx';
+import { ComponentServiceConfig } from '../opt/classes/class.service.config';
+
+@Injectable()
+/**
+ * Define a TableComponent service provider.
+ */
+export class TreeServiceGet extends ComponentServiceGet {
+    /**
+     * Creates an instance of TableService.
+     *
+     * @memberOf TableService
+     */
+    constructor(@Inject(IComponentServiceConfig) config: IComponentServiceConfig, http: Http) {
+        super(config, http, 'tree-service-get');
+    }
+}
